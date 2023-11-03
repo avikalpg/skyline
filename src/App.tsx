867e7a4851386a458b7ab2232c523abdbe455925
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-11-04 01:38:17 +0530

    Replaced the default name and title with GitHub Skyline (fixed indentation)

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          View a 3D model of your GitHub contribution graph.
-          Share it, print it, and more!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div >
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<p>
+					View a 3D model of your GitHub contribution graph.
+					Share it, print it, and more!
+				</p>
+			</header>
+		</div >
+	);
 }
 
 export default App;
