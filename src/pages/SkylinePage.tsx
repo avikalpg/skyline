9702e0634d78dbdd43861f2059c6b2df82a80756
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2023-11-08 10:58:13 +0530

    3d: proof of concept

--- src/pages/SkylinePage.tsx
+++ src/pages/SkylinePage.tsx
@@ -1,5 +1,8 @@
-import { Box, Typography } from "@mui/joy";
+import { Stack, Typography } from "@mui/joy";
 import SingleFoldPageUIWrapper from "../components/SingleFoldPageUIWrapper";
+import { Canvas } from "@react-three/fiber";
+import { Vector3 } from "three";
+import Skyline3d from "../components/Skyline3D";
 
 function SkylinePage() {
 	const data = localStorage.getItem("data");
@@ -14,13 +17,18 @@ function SkylinePage() {
 	const { username, timeline } = JSON.parse(data) as { username: string, timeline: Record<string, number> };
 	return (
 		<SingleFoldPageUIWrapper>
-			<Box>
-				Hello {username}!
-				{Object.entries(timeline).map(([k, v]) => (<Typography key={k}>
-					{k} :: {v}
-				</Typography>))}
-			</Box>
-		</SingleFoldPageUIWrapper>
+			<Stack sx={{ width: '100%', height: '100%' }}>
+				<Typography>
+					Hello {username}!
+				</Typography>
+				<Canvas>
+					{/* <ThreeDObject /> */}
+					<pointLight position={new Vector3(10, 10, 10)} intensity={500} />
+					<ambientLight intensity={0.5} />
+					<Skyline3d data={[[0, 1, 2, 3, 2, 1, 0], [0, 2, 1, 0, 1, 2, 0], [0, 0, 3, 1, 1, 1, 0]]} />
+				</Canvas>
+			</Stack>
+		</SingleFoldPageUIWrapper >
 	)
 }
 
