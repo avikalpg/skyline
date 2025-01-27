c0611467cb5a0c595eaf01b5427dc1127368f7d9
Avikalp Kumar Gupta <avikalpgupta@gmail.com>
2025-01-27 13:29:28 +0530

    1. refactor: update SkylinePage to use userContributionCalendar and handle errors, remove client component 2. feat: add loading state for user contributions and improve date handling 3. fix: update GitHub token environment variable reference

--- src/app/[username]/page.tsx
+++ src/app/[username]/page.tsx
@@ -1,6 +1,44 @@
 import "../../index.css";
-import { ClientOnly } from "./client";
+import { Suspense } from 'react';
+import { getUserContributions } from '../../utils/getUserContributions';
+import { GitHubContributionCalendar } from '../../github-types';
+import SkylinePage from './SkylinePage';
+import { notFound } from 'next/navigation'
+import Loading from './loading'
 
-export default function Page({ params }: { params: { username: string } }) {
-	return <ClientOnly username={params.username} />
+export default function Page({ params, searchParams }: { params: { username: string }, searchParams?: { endDate?: string } }) {
+	const username = params.username;
+	const endDateParam = searchParams?.endDate;
+	const endDate = endDateParam ? new Date(endDateParam) : new Date();
+
+	if (!username) {
+		notFound();
+	}
+
+	return getUserContributions(username, endDate)
+		.then((userContributions: GitHubContributionCalendar) => {
+			if (!userContributions) {
+				console.warn("No contributions found for this user in this period.");
+				return (
+					<Suspense fallback={<Loading />}>
+						<SkylinePage username={username} endDate={endDate} error="No contributions found for this user in this period." />
+					</Suspense>
+				);
+			}
+			else {
+				return (
+					<Suspense fallback={<Loading />}>
+						<SkylinePage username={username} userContributionCalendar={userContributions} endDate={endDate} />
+					</Suspense>
+				);
+			}
+		})
+		.catch((err) => {
+			console.error("Error fetching contributions:", err);
+			return (
+				<Suspense fallback={<Loading />}>
+					<SkylinePage username={username} endDate={endDate} error="Error fetching contributions." />
+				</Suspense>
+			);
+		});
 }
